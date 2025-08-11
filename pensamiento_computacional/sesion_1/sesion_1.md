3818ed29198157152094d547d5574b83f1e93f39
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-08-11 14:41:42 -0300

    feat: Archivos para Pensamiento computacional

---
+++ pensamiento_computacional/sesion_1/sesion_1.md
@@ -0,0 +1,7 @@
+# Sesion 1
+* https://www.freecodecamp.org/news/python-multiline-comment-how-to-comment-out-multiple-lines-in-python/
+
+Para usar python, debo instalarlo.
+* https://www.python.org/downloads/windows/
+
+En VScode arriba a la derecha tengo un boton ▶ para correr mi archivo
---
+++ pensamiento_computacional/sesion_1/sesion_1.py
@@ -0,0 +1,52 @@
+
+
+# Guía de Ejercicios Nº 1:
+"""
+1. Se tiene que explicar a una máquina exactamente cómo servir un vaso de jugo (de los que
+vienen en cartón) de la heladera. Recordando la definición de algoritmo, hacer una
+descripción paso a paso de lo que se tiene que hacer y usar para lograr el objetivo. Pista: No
+vas a necesitar nada de código en este ejercicio, sólo nombrar los pasos.
+
+2. Se tiene que explicar a una máquina exactamente cómo hacer una tostada con queso, pensá
+qué ingredientes se necesitan con sus cantidades, cómo tiene que ser el espacio de trabajo y
+los elementos que va a necesitar usar. Recordando la definición de algoritmo, hacer una
+descripción paso a paso de lo que se tiene que hacer y usar para hacer una tostada con
+queso. Pista: No vas a necesitar nada de código en este ejercicio, sólo nombrar los pasos.
+
+3. Se te pide que organices una colecta de alimentos no perecederos por la Ciudad de Buenos
+Aires. Contamos con algunos automóviles y camionetas de voluntarios, un listado de
+donaciones, listado de los alimentos a donar, la disponibilidad horaria y la dirección en la
+cual se dejan los alimentos. La colecta se realiza en un solo día. ¿Cómo la organizarías?
+
+4. Tenés que enviar invitaciones personalizadas para tu cumpleaños. Cada invitación tiene que
+mencionar el nombre de la persona y la relación que tiene con vos. Contamos con una
+impresora a la que le das el texto a enviar, un listado con los nombres de los invitados y la
+relación que cada uno tiene con vos. ¿Cómo redactarías el texto de la invitación?
+
+5. Se te encargó definir qué datos son necesarios para el registro de estudiantes en un curso de
+inglés. ¿Qué datos crees que deberían ser obligatorios y cuáles opcionales? ¿Y si el curso es
+de cocina?
+
+6. Contás con un listado de cosas a comprar y tenes que ir a un supermercado que cuenta con
+distintas góndolas o pasillos. Cada góndola o pasillo puede contar con varios, uno o ninguno
+de los productos de tu lista. ¿Cuál sería el listado de instrucciones para poder terminar lo
+más rápido posible?
+
+7. A lo largo del cuatrimestre vamos a ver cómo podemos darle instrucciones a la computadora,
+a medida que vayamos aprendiendo a programar. Una vez visto el tutorial de Replit, realice
+su primer programa: hacer que se imprima por pantalla un “¡Hola mundo!”. Ayuda: escribir
+print(“¡Hola mundo!”) y darle play (Run).
+
+8. Esta semana vimos que cuando programamos podemos guardar datos en variables. Teniendo
+en cuenta esto y recordando el concepto de variable que se estudió esta semana, guardar el
+texto “¡Hola mundo!” en una variable e imprimir el texto usando esa variable.
+
+9. Crear otro programa que guarde un número en una variable, y luego lo imprima por pantalla,
+como hicimos con el “¡Hola mundo!” del ejercicio 2, sólo que ahora hay que poner el nombre
+de la variable en lugar del “¡Hola mundo!”.
+
+10. Vamos con otro un poco más complejo. Para el siguiente programa a realizar, se pide hacer
+dos variables que guarden dentro números, y luego sumarlos. El resultado se tendrá que
+guardar en otra variable, y luego imprimir este resultado. Es decir:
+"""
+print("hola mundo")
