6f13638dbe7d6f02a4726d8e626aa1ecbe9ed397
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-10-05 21:08:00 -0300

    feat: Ejercicio 04 de Archivos

--- pensamiento_computacional/sesion_8/ejercicios/ejercicios_8.py
+++ pensamiento_computacional/sesion_8/ejercicios/ejercicios_8.py
@@ -12,18 +12,18 @@ Entonces el archivo debería quedar de la forma:
 # Si leo y escribo, empieza a escribir desde el final, porque al leer, el puntero queda en el final-.
 
 """
-ej_01_file = open('pensamiento_computacional/sesion_8/ejercicios/pregunta.txt', 'r+', encoding='utf-8')
-lines_ej_01_file = ej_01_file.readlines() # array donde cada posicion es una linea del archivo
-print(lines_ej_01_file)
+ej_01_file_read+ = open('pensamiento_computacional/sesion_8/ejercicios/pregunta.txt', 'r+', encoding='utf-8')
+lines_ej_01_file_read+ = ej_01_file_read+.readlines() # array donde cada posicion es una linea del archivo
+print(lines_ej_01_file_read+)
 
 lines_limpio_ej_01 = ''
-for lines in lines_ej_01_file:
+for lines in lines_ej_01_file_read+:
     lines_limpio_ej_01 += (lines.strip('\n')) + ' '
 
 rta_user = input(lines_limpio_ej_01)
 print("rta_user", rta_user)
 
-ej_01_file.write(rta_user + '\n')
+ej_01_file_read+.write(rta_user + '\n')
 """
 
 # 2. En un archivo llamado regalo.txt se tiene la lista de las personas que quieren participar en el regalo de cumpleaños de Sol (en cada línea está el nombre de una persona). El encargado de organizar el regalo es Ale, y quiere saber más información antes de ir a comprarle algo a Sol.
@@ -41,18 +41,18 @@ La función tiene que devolver 5000
 # c. Tomi sabe que si participa Santi, también participa Tomi. Se pide que si Santi está en el archivo de los nombres, se agregue también a Tomi.
 
 """
-ej_02_file = open('pensamiento_computacional/sesion_8/ejercicios/regalo.txt', 'r+', encoding='utf-8')
-lines_ej_02_file = ej_02_file.readlines() # array donde cada posicion es una linea del archivo
-print(lines_ej_02_file) # ['Agus\n', 'Manu\n', 'Santi\n', 'Lorena\n', 'Maria']
+ej_02_file_read+ = open('pensamiento_computacional/sesion_8/ejercicios/regalo.txt', 'r+', encoding='utf-8')
+lines_ej_02_file_read+ = ej_02_file_read+.readlines() # array donde cada posicion es una linea del archivo
+print(lines_ej_02_file_read+) # ['Agus\n', 'Manu\n', 'Santi\n', 'Lorena\n', 'Maria']
 
 monto_total_para_regalo = 0
 
 personas_que_participan_en_el_regalo = []
-for lines in lines_ej_02_file:
+for lines in lines_ej_02_file_read+:
     personas_que_participan_en_el_regalo.append((lines.strip('\n')) + ' ')
     if(lines.count("Santi")): # si lines.count("Santi") > 0
         personas_que_participan_en_el_regalo.append('Tomi')
-        ej_02_file.write('\n' + 'Tomi' + '\n')
+        ej_02_file_read+.write('\n' + 'Tomi' + '\n')
 
 print(personas_que_participan_en_el_regalo)
 
@@ -83,7 +83,8 @@ Pollo
 Fideos
 """
 
-ej_03_file = open('pensamiento_computacional/sesion_8/ejercicios/compras.txt', 'a', encoding='utf-8')
+"""
+ej_03_file_append = open('pensamiento_computacional/sesion_8/ejercicios/compras.txt', 'a', encoding='utf-8')
 lista_de_compras = [];
 item_a_agregar = input('> ¿Qué agrego a la lista de compras? ');
 lista_de_compras.append(item_a_agregar + '\n')
@@ -93,8 +94,8 @@ while(item_a_agregar != 'X'):
         if(item_a_agregar != 'X'):
             lista_de_compras.append(item_a_agregar + '\n')
 
-ej_03_file.writelines(lista_de_compras)
-   
+ej_03_file_append.writelines(lista_de_compras)
+"""
 
 
 # 4. Se tiene un archivo con el siguiente texto:
@@ -116,7 +117,17 @@ ese dicho que tú has dicho
 que han dicho que he dicho yo.
 
 Se pide hacer un programa que pida dos palabras: una que se quiera reemplazar y la palabra por la que se quiera reemplazar, cambie el texto y lo guarde en el archivo otra vez. Por ejemplo, si la función recibe “poco” y “mucho”, reemplaza “poco” por “mucho” todas las veces que aparezca en el texto.
-
 """
 
 
+ej_04_read = open('pensamiento_computacional/sesion_8/ejercicios/ej_04.txt', 'r', encoding='utf-8')
+string_ej_04 = ej_04_read.read() # Todo el contenido en un string. Tiene los saltos de línea implicitos
+# print(string_ej_04)
+
+a_reemplazar = input('Ingrese la palabra que quiere reemplazar ')
+reemplazar_por = input('Ingrese la palabra con la que quiere reemplazar por ')
+
+new = string_ej_04.replace(a_reemplazar, reemplazar_por)
+
+ej_04_write = open('pensamiento_computacional/sesion_8/ejercicios/ej_04.txt', 'w', encoding='utf-8')
+ej_04_write.write(new)
