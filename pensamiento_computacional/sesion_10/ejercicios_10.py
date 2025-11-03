979ef25a6b85af432d8ef63b4669bb8ff8271a4c
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-11-03 08:30:14 -0300

    docs: Descomento código para mejor visualizacion

--- pensamiento_computacional/sesion_10/ejercicios_10.py
+++ pensamiento_computacional/sesion_10/ejercicios_10.py
@@ -62,110 +62,110 @@ print(df)
 Interpretar qué información se guarda en esta tabla y para qué puede servir.
 """
 # ============================================================
-# print()
-# print("df.info() 🔽")
-# print(df.info())
+print()
+print("df.info() 🔽")
+print(df.info())
 
 
 # ============================================================
 # ✅ 2. Mostrar sólo los nombres de las primeras 3 películas del DataFrame.
 # ============================================================
-# print()
-# print("df.head(3) 🔽")
-# print(df.head(3))
+print()
+print("df.head(3) 🔽")
+print(df.head(3))
 
 # ============================================================
 # ✅ 3. Mostrar sólo el director y el género de todas las películas.
 # ============================================================
 
-# print()
-# print("df[['director', 'género']] 🔽")
-# print(df[['director', 'género']])
+print()
+print("df[['director', 'género']] 🔽")
+print(df[['director', 'género']])
 
-# print()
-# print("df.loc[:, ['director', 'género']] 🔽")
-# print(df.loc[:, ['director', 'género']])
+print()
+print("df.loc[:, ['director', 'género']] 🔽")
+print(df.loc[:, ['director', 'género']])
 
 # ============================================================
 # ✅ 4. Mostrar las películas que sean de drama.
 # ============================================================
 
-# print()
-# print("df[df['género'] == 'drama'] 🔽")
-# print(df[df['género'] == 'drama'])
+print()
+print("df[df['género'] == 'drama'] 🔽")
+print(df[df['género'] == 'drama'])
 
 # ============================================================
 # ✅ 5. ¿Qué cantidad de películas hay de cada género?
 # ============================================================
 
-# print()
-# print("df.groupby('género').count() 🟡")
-# print(df.groupby("género").count())
+print()
+print("df.groupby('género').count() 🟡")
+print(df.groupby("género").count())
 
-# # Esto muestra los géneros distintos que hay.
-# print()
-# print("df.groupby('género').count() 🟡")
-# print(df.groupby("género").count())
+# Esto muestra los géneros distintos que hay.
+print()
+print("df.groupby('género').count() 🟡")
+print(df.groupby("género").count())
 
 # # value_counts agrupa y cuenta cada valor de la columna 'género'. Ordena de mayor a menor segun los valores obtenidos
-# print()
-# print("df['género'].value_counts()) 🟢")
-# print(df["género"].value_counts()))
+print()
+print("df['género'].value_counts()) 🟢")
+print(df["género"].value_counts())
 
-# # Esto agrupa el DataFrame por 'género' y luego cuenta cuántas filas hay en cada grupo.
-# print()
-# print("df.groupby('género')['género'].count()) 🟢")
-# print(df.groupby('género')['género'].count()))
+# agrupa el DataFrame por 'género' y luego cuenta cuántas filas hay en cada grupo.
+print()
+print("df.groupby('género')['género'].count()) 🟢")
+print(df.groupby('género')['género'].count())
 
 # ============================================================
 # ✅ 6. Mostrar las películas que tengan puntaje entre 6 y 8 y cuyo año de estreno sea anterior a los 2000.
 # ============================================================
 
-# print()
-# print("df[df['puntaje'].between(6, 8) & (df['año'] < 2000)] 🟢")
-# print(df[df["puntaje"].between(6, 8) & (df["año"] < 2000)])
+print()
+print("df[df['puntaje'].between(6, 8) & (df['año'] < 2000)] 🟢")
+print(df[df["puntaje"].between(6, 8) & (df["año"] < 2000)])
 
 # ============================================================
 # ✅ 7. Mostrar las películas que no hayan sido puntuadas
 # (que el puntaje tenga un valor nulo).
 # ============================================================
 
-# print()
-# print("df[df['puntaje'].isnull()] 🟢")
-# print(df[df["puntaje"].isnull()])
+print()
+print("df[df['puntaje'].isnull()] 🟢")
+print(df[df["puntaje"].isnull()])
 
 
 # ============================================================
 # ✅ 8. Calcular el promedio del puntaje de todas las películas.
 # ============================================================
 
-# print()
-# print("df.groupby('género')['género'].mean() 🟢")
-# print(df.groupby('género')['puntaje'].mean())
+print()
+print("df.groupby('género')['género'].mean() 🟢")
+print(df.groupby('género')['puntaje'].mean())
 
 # ============================================================
 # ✅ 9. Ordenar las películas en orden alfabético descendente.
 # ============================================================
 
-# print()
-# print("df.sort_values(by=['nombre']) 🟢")
-# print(df.sort_values(by=["nombre"]))
+print()
+print("df.sort_values(by=['nombre']) 🟢")
+print(df.sort_values(by=["nombre"]))
 
 # ============================================================
 # ✅ 10. Mostrar las 3 películas más antiguas.
 # ============================================================
 
-# print()
-# print("df.sort_values(by='año').head(3) 🟢")
-# print(df.sort_values(by="año").head(3))
+print()
+print("df.sort_values(by='año').head(3) 🟢")
+print(df.sort_values(by="año").head(3))
 
 # ============================================================
 # ✅ 11. Mostrar sólo el nombre y el año de las 3 películas más nuevas.
 # ============================================================
 
-# print()
-# print("df[['nombre', 'año']].sort_values(by='año', ascending=False).head(3) 🟢")
-# print(df[["nombre", "año"]].sort_values(by="año", ascending=False).head(3))
+print()
+print("df[['nombre', 'año']].sort_values(by='año', ascending=False).head(3) 🟢")
+print(df[["nombre", "año"]].sort_values(by="año", ascending=False).head(3))
 
 # ============================================================
 # 12. Agregar una columna que indique si la película fue vista, o no. Una película fue vista cuando tiene puntaje no nulo.
